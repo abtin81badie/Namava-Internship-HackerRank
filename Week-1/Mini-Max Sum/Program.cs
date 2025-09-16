bace251e758196696ff405e9173f6520ef827bc5
abtin81badie <abtinbadiee1328@gmail.com>
2025-09-16 09:16:08 +0330

    feat: Add solution for Week-1/Mini-Max Sum/Program.cs, Week-1/Plus Minus/Program.cs.

---
+++ Week-1/Mini-Max Sum/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+        double maxSum = 0;
+        double minSum = 0;
+
+        for (int i = 0; i < arr.Count; i++) 
+        {
+            if (i == 0) {
+                minSum += arr[i];
+            } else if (i == arr.Count - 1)
+            {
+                maxSum += arr[i];
+            }
+            else
+            {
+                maxSum += arr[i];
+                minSum += arr[i];
+            }
+        }
+
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
