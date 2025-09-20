1c0811cdec756d5aaf90a9514f9393069c3d64c3
abtin81badie <abtinbadiee1328@gmail.com>
2025-09-20 08:41:27 +0330

    feat: Add solution for Lonely Integer.

---
+++ Week-2/Lonely Integer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int lonelyinteger(List<int> a)
+    {
+        var uniqueNumbers = a.GroupBy(n => n)
+                            .Where(g => g.Count() == 1)
+                            .Select(g => g.First());
+    
+        return uniqueNumbers.First();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+
+        Console.WriteLine(result);      
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
