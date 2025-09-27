f973cc612993848e301ea6cea485791d41c58d50
abtin81badie <abtinbadiee1328@gmail.com>
2025-09-27 11:16:25 +0330

    fix: fix the source file.

--- Week-1/Find The Median/Program.cs
+++ Week-1/Find The Median/Program.cs
@@ -12,38 +12,27 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
+
+
 class Result
 {
-    public static List<int> matchingStrings(List<string> strings, List<string> queries)
+    public static int findMedian(List<int> arr)
     {
-        Dictionary<string,int> stringsFreq = new Dictionary<string,int>();
+        arr.Sort();
+        int numberOfElements = arr.Count;
+        int index = 0;
 
-        foreach (string s in strings) 
+        if (numberOfElements % 2 == 0)
         {
-            if (!stringsFreq.ContainsKey(s))
-            {
-                stringsFreq.Add(s, 1);
-            } 
-            else
-            {
-                stringsFreq[s]++;
-            }
+            index = numberOfElements / 2 - 1; 
+        }
+        else
+        {
+            index = (numberOfElements / 2);
         }
 
-        List<int> result = new List<int>();
-        foreach (string q in queries) 
-        {
-            if (stringsFreq.ContainsKey(q))
-            {
-                result.Add(stringsFreq[q]);
-            }
-            else 
-            {
-                result.Add(0);
-            }
-        }
+       return arr[index];
 
-        return result;
     }
 
 }
@@ -52,32 +41,16 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        List<string> strings = new List<string>();
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
-        for (int i = 0; i < stringsCount; i++)
-        {
-            string stringsItem = Console.ReadLine();
-            strings.Add(stringsItem);
-        }
+        int result = Result.findMedian(arr);
 
-        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
-
-        List<string> queries = new List<string>();
-
-        for (int i = 0; i < queriesCount; i++)
-        {
-            string queriesItem = Console.ReadLine();
-            queries.Add(queriesItem);
-        }
-
-        List<int> res = Result.matchingStrings(strings, queries);
-
-        Console.WriteLine(String.Join("\n", res));
-        //textWriter.WriteLine(String.Join("\n", res));
+        Console.WriteLine(result);
+        //textWriter.WriteLine(result);
 
         //textWriter.Flush();
         //textWriter.Close();
