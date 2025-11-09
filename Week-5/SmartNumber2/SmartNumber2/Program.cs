723c2a0a31bf350c868ea4443357b1ae6660422d
abtin81badie <abtinbadiee1328@gmail.com>
2025-11-09 22:23:58 +0330

    feat: Add SmartNumber2 Solution C#.

---
+++ Week-5/SmartNumber2/SmartNumber2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Result
+{
+    private static void CheckConstraints(int number)
+    {
+        if (number < 1 || number > Math.Pow(10, 5))
+            throw new ArgumentOutOfRangeException(nameof(number),
+                $"The number must be between 1 and 10_000. Provided value: {number}");
+
+    }
+
+    public static bool IsSmartNumber(int number)
+    {
+        CheckConstraints(number);
+
+        int val = (int)Math.Sqrt(number);
+        return val * val == number;
+    }
+
+}
+
+
+class Solution
+{
+    public static void Main()
+    {
+        // Read the number of test cases
+        int testCases = int.Parse(Console.ReadLine());
+
+        // Process each test case
+        for (int i = 0; i < testCases; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Result.IsSmartNumber(num) ? "YES" : "NO");
+        }
+    }
+}
