61a731f9d96deb017dd989b5119f3abdf9b2add4
Abtin Badiee <117802598+abtin81badie@users.noreply.github.com>
2025-11-29 14:47:36 +0330

    feat: Add solution for Palindrome Index.

---
+++ Week-6/Palindrome Index/Palindrome Index/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+    private static void CheckConstraints(string s)
+    {
+        if (s.Length < 1 || s.Length > 5 + Math.Pow(10, 5))
+            throw new ArgumentException(
+                $"Input string length must be between 1 and 5 + 10^5 characters, inclusive. Current length is {s.Length}.",
+                nameof(s));
+    }
+
+    private static bool IsPalindrome(string s, int start, int end)
+    {
+        while (start < end)
+        {
+            if (s[start] != s[end])
+                return false;
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static int palindromeIndex(string s)
+    {
+        CheckConstraints(s);
+
+        if (s.SequenceEqual(s.Reverse()))
+            return -1;
+
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (s[left] == s[right])
+            {
+                left++;
+                right--;
+            }
+            else
+            {
+                if (IsPalindrome(s, left + 1, right))
+                    return left;
+
+                if (IsPalindrome(s, left, right - 1))
+                    return right;
+
+                break;
+            }
+        }
+
+        return -1;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = Result.palindromeIndex(s);
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
