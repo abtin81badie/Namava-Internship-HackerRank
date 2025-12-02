37dfb42a93ceab478e1ec2fb02c4d8e25609d501
Abtin Badiee <117802598+abtin81badie@users.noreply.github.com>
2025-12-02 09:33:27 +0330

    feat: Add solution for Goodland Electricity.

---
+++ Week-7/GoodlandElectricity/GoodlandElectricity/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pylons' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+    private struct LineSegment
+    {
+        public int StartPoint;
+        public int EndPoint;
+
+        public LineSegment(int startPoint, int endPoint)
+        {
+            this.StartPoint = startPoint;
+            this.EndPoint = endPoint;
+        }
+
+    }
+
+    public static int Pylons(int k, List<int> arr)
+    {
+        var lineSegments = new List<LineSegment>();
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (arr[i] == 1)
+            {
+                int startPoint = Math.Max(0, i - k + 1);
+                int endPoint = Math.Min(arr.Count - 1, i + k - 1);
+
+                lineSegments.Add(new LineSegment(startPoint, endPoint));
+            }
+        }
+
+        lineSegments.Sort((a, b) =>
+        {
+            int result = a.StartPoint.CompareTo(b.StartPoint);
+            return result;
+        });
+
+        var pylonsCount = 0;
+        var currentUncoveredIndex = 0;
+        var segmentIndex = 0;
+
+        while (currentUncoveredIndex < arr.Count)
+        {
+            var bestReach = -1;
+            var foundSegment = false;
+
+            while (segmentIndex < lineSegments.Count &&
+                    lineSegments[segmentIndex].StartPoint <= currentUncoveredIndex)
+            {
+                if (lineSegments[segmentIndex].EndPoint > bestReach)
+                {
+                    bestReach = lineSegments[segmentIndex].EndPoint;
+                    foundSegment = true;
+                }
+                segmentIndex++;
+            }
+
+            if (!foundSegment || bestReach < currentUncoveredIndex)
+                return -1;
+
+            pylonsCount++;
+
+            currentUncoveredIndex = bestReach + 1;
+        }
+
+        return pylonsCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.Pylons(k, arr);
+
+        Console.WriteLine(result);
+
+    }
+}
--- Week-7/New Year Chaos/New Year Chaos/Program.cs
+++ Week-7/New Year Chaos/New Year Chaos/Program.cs
@@ -32,6 +32,41 @@ class Result
 
         var bribes = 0;
 
+        // Iterate backwards from the end of the line
+        for (int i = q.Count - 1; i >= 0; i--)
+        {
+            // The value that *should* be at this position (1-based index)
+            int expected = i + 1;
+
+            // If the person currently at i is not the expected person
+            if (q[i] != expected)
+            {
+                // Check if the expected person is 1 spot ahead (at i-1)
+                if (i - 1 >= 0 && q[i - 1] == expected)
+                {
+                    bribes++;
+                    Swap(q, i, i - 1);
+                }
+                // Check if the expected person is 2 spots ahead (at i-2)
+                else if (i - 2 >= 0 && q[i - 2] == expected)
+                {
+                    bribes += 2;
+                    // Move the person from i-2 to i requires two swaps:
+                    // 1. Swap (i-2) with (i-1)
+                    // 2. Swap (i-1) with (i)
+                    Swap(q, i - 2, i - 1);
+                    Swap(q, i - 1, i);
+                }
+                // If the expected person is not found in the previous 2 spots,
+                // it means they moved more than 2 spots, which is impossible.
+                else
+                {
+                    Console.WriteLine("Too chaotic");
+                    return;
+                }
+            }
+        }
+
         for (int i = 0; i < q.Count; i++) 
         {
             if (q[i] - (i + 1) > 2)
