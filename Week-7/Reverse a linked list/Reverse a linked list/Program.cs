8061c89de7b1a43ed0d0481e6afe882557b5add2
Abtin Badiee <117802598+abtin81badie@users.noreply.github.com>
2025-12-07 23:36:16 +0330

    feat: Add solutioin for Reverse a linked list.

---
+++ Week-7/Reverse a linked list/Reverse a linked list/Program.cs
@@ -0,0 +1,146 @@
+﻿#nullable disable
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep)
+    {
+        while (node != null)
+        {
+            Console.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                Console.Write(sep);
+            }
+        }
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'reverse' function below.
+         *
+         * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+         * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+         */
+
+        /*
+         * For your reference:
+         *
+         * SinglyLinkedListNode
+         * {
+         *     int data;
+         *     SinglyLinkedListNode next;
+         * }
+         *
+         */
+        private static void CheckConstraints(SinglyLinkedListNode llist)
+        {
+            while(llist != null)
+            {
+                if (llist.data < 1
+                    || llist.data > 1000)
+                    throw new ArgumentException($"List element {llist.data} is out of bounds. Elements must be between 1 and 1000.");
+            }
+        } 
+
+        public static SinglyLinkedListNode Reverse(SinglyLinkedListNode llist)
+        {
+            SinglyLinkedListNode? previousNode = null;
+            SinglyLinkedListNode currentNode = llist;
+            SinglyLinkedListNode? nextNode = null;
+
+            while (currentNode != null)
+            {
+                nextNode = currentNode.next;
+                currentNode.next = previousNode;
+
+                previousNode = currentNode;
+                currentNode = nextNode;
+            }
+
+            return previousNode;
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        int tests = Convert.ToInt32(Console.ReadLine());
+
+        for (int testsItr = 0; testsItr < tests; testsItr++)
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            SinglyLinkedListNode llist1 = Result.Reverse(llist.head);
+
+            PrintSinglyLinkedList(llist1, " ");
+            Console.WriteLine();
+        }
+    }
+}
